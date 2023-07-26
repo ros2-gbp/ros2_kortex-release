5800c8e3b56a2a001e432833628ef3da8be2b691
Alexander Moriarty <alex.moriarty@picknik.ai>
2023-07-26 17:28:00 -0300

    Revert "Trimmed the branch to only the kortex_driver sub directory"
    This reverts commit 510eebcdc4d3829a8652171e8074e613436626ee.

---
+++ kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/robot.launch.py
@@ -0,0 +1,84 @@
+# Copyright (c) 2023 PickNik, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_demo_launch
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_ip",
+            description="IP address by which the robot can be reached.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper_max_velocity",
+            default_value="100.0",
+            description="Max velocity for gripper commands",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper_max_force",
+            default_value="100.0",
+            description="Max force for gripper commands",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            description="Use simulated clock",
+        )
+    )
+
+    # Initialize Arguments
+    robot_ip = LaunchConfiguration("robot_ip")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    gripper_max_velocity = LaunchConfiguration("gripper_max_velocity")
+    gripper_max_force = LaunchConfiguration("gripper_max_force")
+
+    launch_arguments = {
+        "robot_ip": robot_ip,
+        "use_fake_hardware": use_fake_hardware,
+        "gripper": "robotiq_2f_85",
+        "gripper_joint_name": "robotiq_85_left_knuckle_joint",
+        "dof": "7",
+        "gripper_max_velocity": gripper_max_velocity,
+        "gripper_max_force": gripper_max_force,
+    }
+
+    moveit_config = (
+        MoveItConfigsBuilder("gen3", package_name="kinova_gen3_7dof_robotiq_2f_85_moveit_config")
+        .robot_description(mappings=launch_arguments)
+        .to_moveit_configs()
+    )
+
+    moveit_config.moveit_cpp.update({"use_sim_time": LaunchConfiguration("use_sim_time")})
+    return generate_demo_launch(moveit_config)
